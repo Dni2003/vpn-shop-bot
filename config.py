a89036600ce3f9585af6e757003b0cc9bcaced60
Dni2003 <115918319+Dni2003@users.noreply.github.com>
2026-07-31 04:28:11 +0330

    Add CARD_NUMBER to configuration

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
     DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///database.db")
-    
+    CARD_NUMBER = os.getenv("CARD_NUMBER", "6219861801306746 دانیال بدری")
     # تنظیمات مالی
     CURRENCY = "IRR"  # یا "USD"
     VAT_PERCENT = 0  # درصد مالیات
